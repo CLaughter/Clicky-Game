4ba0d0f350ee6490d62e03725992fd75f27790ac
Chris Laughter <chrislaugh@gmail.com>
2020-01-11 22:45:21 -0700

    Updates

---
+++ precache-manifest.a4f34154674ed2cde71d3a35ffe1e4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30964ea6e0117eb9eaa813facdaff537",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e1c03bd2ae1c14ad07be",
+    "url": "/clicky-game/static/css/main.82d277ad.chunk.css"
+  },
+  {
+    "revision": "24174d323ac21f04f040",
+    "url": "/clicky-game/static/js/2.49212cf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.49212cf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1c03bd2ae1c14ad07be",
+    "url": "/clicky-game/static/js/main.5a3d8d50.chunk.js"
+  },
+  {
+    "revision": "0f9f9acf91bc4b9d055b",
+    "url": "/clicky-game/static/js/runtime-main.c289e6cb.js"
+  }
+]);
